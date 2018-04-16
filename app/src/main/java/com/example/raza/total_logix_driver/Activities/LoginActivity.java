9a3a866ba7c8639d5d4bbc22f28876c22fe130be
raza <adilraza@gmail.com>
2018-04-16 00:01:59 -0700

    Start Project Driver

---
+++ app/src/main/java/com/example/raza/total_logix_driver/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.raza.total_logix_driver.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.raza.total_logix_driver.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
